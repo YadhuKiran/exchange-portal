afc4227f0523f82e4e76829ab9589cca59335483
Yadhu Kiran <kiranyadhu5@gmail.com>
2026-08-15 22:06:31 +0800

    Correct closing brace in login.php
    Fix syntax error in login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/includes/init.php';
 
 if (is_logged_in()) {
     redirect('/');
-}
+} wirklich
 
 $error = '';
 $selectedRole = $_POST['role'] ?? $_GET['role'] ?? 'student';
